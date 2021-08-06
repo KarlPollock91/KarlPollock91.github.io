c6fb93f5cfad02338cf698d286050148ab90fc7a
KarlPollock91 <KarlPollock91@gmail.com>
2021-08-06 18:23:52 +1200

    Can now adjust date range of data

--- assets/js/covid.js
+++ assets/js/covid.js
@@ -2,10 +2,20 @@ const graphWidth = 550;
 const graphHeight = 400;
 var numDays = 30;
 var query = `https://data.nsw.gov.au/data/api/3/action/datastore_search_sql?sql=SELECT * from "21304414-1ff1-4243-a5d2-f52778048b29" ORDER BY notification_date DESC`
+var mapsReady = false;
+
+var dailyNewCases = [];
+var dailyNewCasesSDA = [];
+var firstRateOfChange = [];
+var firstRateOfChangeSDA = [];
+var secondRateOfChange = [];
+var secondRateOfChangeSDA = [];
+
 
 fetch(query).then((response) => {
     return response.json();
 }).then((json) => {
+    
     var dailyNewCasesDict = {};
 
     //Count each case by date
@@ -19,7 +29,6 @@ fetch(query).then((response) => {
     }
     
     //Place data into orderable structure
-    var dailyNewCases = [];
 
     for (let key in dailyNewCasesDict) {
         dailyNewCases.push([key, dailyNewCasesDict[key]])
@@ -28,47 +37,16 @@ fetch(query).then((response) => {
         return a[0] - b[0];
     }).reverse();
 
-    var dailyNewCasesSDA = calculateSevenDayAverage(dailyNewCases);
-    var firstRateOfChange = calculateDailyRateOfChange(dailyNewCases);
-    var firstRateOfChangeSDA = calculateSevenDayAverage(firstRateOfChange);
-    var secondRateOfChange = calculateDailyRateOfChange(firstRateOfChange);
-    var secondRateOfChangeSDA = calculateSevenDayAverage(secondRateOfChange);
+    dailyNewCasesSDA = calculateSevenDayAverage(dailyNewCases);
+    firstRateOfChange = calculateDailyRateOfChange(dailyNewCases);
+    firstRateOfChangeSDA = calculateSevenDayAverage(firstRateOfChange);
+    secondRateOfChange = calculateDailyRateOfChange(firstRateOfChange);
+    secondRateOfChangeSDA = calculateSevenDayAverage(secondRateOfChange);
     
 
     google.charts.load('current', {'packages':['corechart']});
     // Set a callback to run when the Google Visualization API is loaded.
     google.charts.setOnLoadCallback(readyToDraw);
-
-    function readyToDraw() {
-        drawChart(dailyNewCases, 'Daily New Cases of Covid-19 in NSW', 'Daily new cases', 'row1graph1');
-        drawChart(dailyNewCasesSDA, 'Daily New Cases of Covid-19 in NSW (Seven Day Average)', 'Daily new cases', 'row1graph2');
-        drawChart(firstRateOfChange, 'Rate of Change of Daily New Cases of Covid-19 in NSW', 'Rate of Change', 'row2graph1');
-        drawChart(firstRateOfChangeSDA, 'Rate of Change of Daily New Cases of Covid-19 in NSW (Seven Day Average)', 'Rate of Change', 'row2graph2');
-        drawChart(secondRateOfChange, 'Second Rate of Change of Daily New Cases of Covid-19 in NSW', 'Rate of Change', 'row3graph1');
-        drawChart(secondRateOfChangeSDA, 'Second Rate of Change of Daily New Cases of Covid-19 in NSW (Seven Day Average)', 'Rate of Change', 'row3graph2');
-    }
-
-    function drawChart(data, title, yAxis, div) {
-        //Row1Chart1 Unsmoothed daily new cases 
-        var graphData = new google.visualization.DataTable();
-        graphData.addColumn('string', 'Date');
-        graphData.addColumn('number', yAxis);
-        graphData.addRows(data.slice(data.length - numDays));
-           
-        var options = {'title' : title,
-            hAxis: {
-                title: 'Month'
-            },
-            vAxis: {
-                title: yAxis
-            },   
-            'width':graphWidth,
-            'height':graphHeight	  
-        };
-
-        var chart = new google.visualization.LineChart(document.getElementById(div));
-        chart.draw(graphData, options);
-    }
 
     function calculateSevenDayAverage(data) {
         sevenDayAverage = [];
@@ -95,3 +73,44 @@ fetch(query).then((response) => {
     console.log(err);
 });
 
+function readyToDraw() {
+    mapsReady = true;
+    drawChart(dailyNewCases, 'Daily New Cases of Covid-19 in NSW', 'Daily new cases', 'row1graph1');
+    drawChart(dailyNewCasesSDA, 'Daily New Cases of Covid-19 in NSW (Seven Day Average)', 'Daily new cases', 'row1graph2');
+    drawChart(firstRateOfChange, 'Rate of Change of Daily New Cases of Covid-19 in NSW', 'Rate of Change', 'row2graph1');
+    drawChart(firstRateOfChangeSDA, 'Rate of Change of Daily New Cases of Covid-19 in NSW (Seven Day Average)', 'Rate of Change', 'row2graph2');
+    drawChart(secondRateOfChange, 'Second Rate of Change of Daily New Cases of Covid-19 in NSW', 'Rate of Change', 'row3graph1');
+    drawChart(secondRateOfChangeSDA, 'Second Rate of Change of Daily New Cases of Covid-19 in NSW (Seven Day Average)', 'Rate of Change', 'row3graph2');
+}
+
+function drawChart(data, title, yAxis, div) {
+    //Row1Chart1 Unsmoothed daily new cases 
+    var graphData = new google.visualization.DataTable();
+    graphData.addColumn('string', 'Date');
+    graphData.addColumn('number', yAxis);
+    graphData.addRows(data.slice(data.length - numDays));
+       
+    var options = {'title' : title,
+        hAxis: {
+            title: 'Month'
+        },
+        vAxis: {
+            title: yAxis
+        },   
+        'width':graphWidth,
+        'height':graphHeight	  
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById(div));
+    chart.draw(graphData, options);
+}
+
+function inputSubmit() {
+    if (mapsReady) {
+        console.log(document.getElementById("num-days-input").value);
+        console.log(parseInt(document.getElementById("num-days-input").value));
+        numDays = Math.min(parseInt(document.getElementById("num-days-input").value), secondRateOfChangeSDA.length);
+        console.log(numDays);
+        readyToDraw();
+    }
+}
